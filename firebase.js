e19e277c1710a7f06683c900abc801f848635c20
hoothe17 <hoothe17@gmail.com>
2026-02-01 12:12:20 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "YOUR_KEY",
+  authDomain: "YOUR_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
